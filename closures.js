39085941ee852212b85f5907546d5a2339071a61
Mattson Compton <mcompton@Mattsons-MacBook-Pro.local>
2019-01-31 15:35:04 -0700

    first draft

--- closures.js
+++ closures.js
@@ -24,13 +24,13 @@ function outer() {
   
 // Code Here
 
-
+let inner = outer();
 
 //Once you do that, invoke inner.
 
 //Code Here
 
-
+console.log(inner());
 
 ////////// PROBLEM 2 //////////
 
@@ -52,7 +52,10 @@ function callFriend(name) {
 */
 
 //Code Here
+ 
+let callJake = callFriend('Jake')
 
+console.log(callJake('435-555-9248'))
 
 
 ////////// PROBLEM 3 //////////
@@ -63,14 +66,22 @@ function callFriend(name) {
 
 //Code Here
 
-
+const makeCounter = () => {
+  let counting = 0;
+  return () => {
+    counting++
+    console.log(counting)
+    return counting
+  }
+  
+};
 
 //Uncomment this once you make your function
-//   var count = makeCounter();
-//   count(); // 1
-//   count(); // 2
-//   count(); // 3
-//   count(); // 4
+  var count = makeCounter();
+  count(); // 1
+  count(); // 2
+  count(); // 3
+  count(); // 4
 
 
 
@@ -89,15 +100,22 @@ function counterFactory(value) {
   // Code here.
 
   return {
-
+    inc:()=>{
+      value++
+      return value
+    },
+    dec:()=>{
+      value--
+      return value
+    }
   };
 }
 
 counter = counterFactory(10);
-// counter.inc() // 11
-// counter.inc() // 12
-// counter.inc() // 13
-// counter.dec() // 12
+counter.inc() // 11
+counter.inc() // 12
+counter.inc() // 13
+counter.dec() // 12
 
 
 
@@ -113,9 +131,11 @@ function motivation( firstname, lastname ) {
   var welcomeText = "You're doing awesome, keep it up";
 
   // code message function here.
-
+  function message(){
+    return `${welcomeText} ${firstname} ${lastname}.`
+  };
   //Uncommment this to return the value of your message function
-  //return message;
+  return message;
 }
 
 var greeting = motivation('Billy', 'Bob'); // 'You're doing awesome keep it up Billy Bob.
@@ -144,6 +164,7 @@ var module = (function() {
   // outside our lexical scope
   return {
     // Code here.
+    
   };
 })();
 
